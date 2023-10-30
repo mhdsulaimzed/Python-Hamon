53e18379fc2e0211a0ac66e7cec56eb5325f7d70
sulaim <sulaimsaed@gmail.com>
2023-10-30 15:30:50 +0530

    this function is to check the palindrome
    This function is to check whether the iput string is palindrome or not

--- exersise1.py
+++ exersise1.py
@@ -8,13 +8,35 @@
            instead i
         d. Otherwise, it will just print i """
 
-def fizzbizz(self):
-    n=int(input("enter a number"))
-    for i in range(1,n+1):
-        if i%15 == 0 : print("fizzbizz")
-        elif i%3 == 0 and i%15 !=0: print("fizz")
-        elif i%5 == 0 and i%15 !=0:print("bizz")
-        
-        else:print(i," ")
+# def fizzbizz(self):
+#     n=int(input("enter a number"))
+#     for i in range(1,n+1):
+#         if i%15 == 0 : print("fizzbizz")
+#         elif i%3 == 0 and i%15 !=0: print("fizz")
+#         elif i%5 == 0 and i%15 !=0:print("bizz")
+#         else:print(i," ")
 
-fizzbizz(" ")
+# fizzbizz(" ")
+
+
+
+"""Write a python function called palindrome that receives a string s as an
+       argument. It will return True if s is a palindrome and False if
+       not. A palindrome is a word that will read the same from left
+       to right and right to left. e.g. "dad", "abba"""
+  
+
+def palindrome(word):
+    s=word[::-1]
+    if word == s:print(True)
+    else: print(False) 
+        
+
+
+
+palindrome("malayalam")
+palindrome("nivin")
+palindrome("sita")
+    
+
+
