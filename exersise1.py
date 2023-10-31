c49a3e538892fd51df2ab9f51ce38b5f4b39c72c
sulaim <sulaimsaed@gmail.com>
2023-10-31 16:33:54 +0530

    uncommenting codes and cleaned up and made it readable as per sir's advice
    fizzbizz was not readble and made it readable with proper indetations

--- exersise1.py
+++ exersise1.py
@@ -8,15 +8,18 @@
            instead i
         d. Otherwise, it will just print i """
 
-# def fizzbizz(self):
-#     n=int(input("enter a number"))
-#     for i in range(1,n+1):
-#         if i%15 == 0 : print("fizzbizz")
-#         elif i%3 == 0 and i%15 !=0: print("fizz")
-#         elif i%5 == 0 and i%15 !=0:print("bizz")
-#         else:print(i," ")
+def fizzbizz(n):
+    for i in range(1,n+1):
+        if i%15 == 0 : 
+            print("fizzbizz")
+        elif i%3 == 0 : 
+            print("fizz")
+        elif i%5 == 0 :
+            print("bizz")
+        else:
+            print(i," ")
 
-# fizzbizz(" ")
+
 
 
 
@@ -26,17 +29,17 @@
        to right and right to left. e.g. "dad", "abba"""
   
 
-# def palindrome(word):
-#     reversedword=word[::-1]
-#     if word == reversedword :print(True)
-#     else: print(False) 
+
+
+def palindrome(word):
+    reversedword=word[::-1]
+    if word == reversedword :print(True)
+    else: print(False) 
         
 
 
 
-# palindrome("malayalam")
-# palindrome("nivin")
-# palindrome("sita")
+
 
 
 """Write a python function called panagram that receives a string s as an
@@ -45,24 +48,23 @@
        alphabet. e.g. "the quick brown fox jumps over the lazy dog" """
 
 
-# def panagram(string):
+def panagram(string):
 
-#     stringList=list(string.lower())
-#     stringSet=set(stringList)
-#     panagramList=[]
+    stringList=list(string.lower())
+    stringSet=set(stringList)
+    panagramList=[]
     
-#     for i in stringSet:
-#         if  i in "abcdefghijklmnopqrstuvwxyz" :
-#             panagramList.append(i)
+    for i in stringSet:
+        if  i in "abcdefghijklmnopqrstuvwxyz" :
+            panagramList.append(i)
     
-#     if len(panagramList) == 26: print(True)
-#     else: print(False)
+    if len(panagramList) == 26: print(True)
+    else: print(False)
         
 
         
 
 
-# panagram("the quick brown fox @ jumps over the lazy dog")
     
 """ Write a function freq that will take a string s as input and
        return a dictionary. The keys of the dictionary will be the
@@ -71,9 +73,6 @@
        .count method of strings. (ie. s.count()) """
 
 def freq(string):
-
-    
-    
 
     frequencyDict = {}
 
@@ -90,7 +89,6 @@ def freq(string):
 
 
 
-freq("fjha gsfs vs##7EB")
 
 
 
