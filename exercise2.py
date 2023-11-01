b5a8d0f9b0963dae8cd97233819cf67628b4e71c
sulaim <sulaimsaed@gmail.com>
2023-11-01 19:38:57 +0530

    Tried division with numbers ending with zero Red :Couldnt handle zerodivission error

--- exercise2.py
+++ exercise2.py
@@ -15,8 +15,11 @@ def evaluate(string):
                 stack.append(opr1*opr2) 
             
 
-            elif i == "/":
+            elif i == "/" and opr1>0:
                 stack.append(opr2//opr1)
+
+            
+                
 
             elif i == "%":
                 stack.append(opr2%opr1)
@@ -30,7 +33,7 @@ def evaluate(string):
     return stack.pop()
 
 
-print(evaluate("62%"))
+print(evaluate("40/"))
 
 
 
